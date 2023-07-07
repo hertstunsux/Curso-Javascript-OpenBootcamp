c181561b8d970a58fb54f8bbcd3d9a08f8da7d5e
hertstunsux <sebas__pf@hotmail.com>
2023-07-07 04:40:03 -0400

    Ejercicio 2

---
+++ Ejercicio 2/main.js
@@ -0,0 +1,8 @@
+const array = ["Sebastián Pereira Flores", 25, true, new Date(1997, 6, 25), 
+    { titulo: "El pequeño vampiro", 
+      autor: "Angela Sommer-Bodenburg", 
+      fecha: new Date(2016, 0, 14), 
+      url: "https://www.amazon.es/El-peque%C3%B1o-vampiro-Angela-Sommer-Bodenburg/dp/8491221220/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=KJMGV8R3B8CF&keywords=el+peque%C3%B1o+vampiro&qid=1688718541&s=books&sprefix=el+peque%C3%B1o+vampiro%2Cstripbooks%2C497&sr=1-1"
+    } 
+]
+console.log(array);
